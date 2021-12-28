c2368b7aac6f02338509931ee90c173b4f450234
jeffrey7377 <fengjiahe1999@gmail.com>
2021-12-28 15:21:42 -0800

    final changes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,6 +4,13 @@ from .models import Course, Lesson, Instructor, Learner
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -28,3 +35,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
